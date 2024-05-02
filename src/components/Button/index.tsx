b5f8378488b0b3fc449bec9bf265302deb44fe0e
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-05-02 18:01:26 -0300

    using svg

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import { Button as ButtonNativeBase, IIconButtonProps,Heading } from "native-base";
+
+export function Button() {
+  return (
+  <ButtonNativeBase>
+    <Heading>ButtonNativeBase</Heading>
+  </ButtonNativeBase>
+  );
+}

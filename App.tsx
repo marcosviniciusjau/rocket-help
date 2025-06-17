04c8f7f45df50819519619547c81315893beabe2
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2025-06-17 12:07:40 -0300

    install

--- App.tsx
+++ App.tsx
@@ -1,3 +1,16 @@
+<<<<<<< HEAD
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+
+import {THEME} from './src/styles/theme';
+
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
+=======
 import { NativeBaseProvider, StatusBar } from "native-base";
 import {
   useFonts,
@@ -12,6 +25,7 @@ import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+>>>>>>> aac4a397acfa2152b1890a79f49c57addc638dc1
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -20,8 +34,16 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+<<<<<<< HEAD
+      
+      { fontsLoaded ? <SignIn/> : <Loading/> }
+    </NativeBaseProvider>
+  );
+}
+=======
 
       {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
+>>>>>>> aac4a397acfa2152b1890a79f49c57addc638dc1
---
+++ README.md
@@ -0,0 +1,11 @@
+## Rockethelp
+
+*** Rockethelp é um aplicativo em React Native para lojas de informáticas poderem registar as demandas de seus clientes. Utiliza um banco de dados Firebase
+
+## Funcionalidades
+
+- Autenticação de usuários
+- Registar demandas
+- Visualizar demandas
+- Abrir chamados
+- Ver os chamados em tempo real
